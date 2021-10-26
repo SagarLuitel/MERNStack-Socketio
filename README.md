00d05eabb058d24cfa1d787c2f74bfe3c50ab9b9
Sagar Luitel <luitelsgr@gmail.com>
2021-10-26 14:16:14 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple project made using socket io and event emitter to show implementation of realtime notificatification in mern stack without using database. Project frontend skeletion is similar to instagram
---
+++ client/src/data.js
@@ -0,0 +1,24 @@
+export const posts = [
+    {
+      id: 1,
+      username:"john",
+      fullname: "John Keller",
+      userImg: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      postImg: "https://images.pexels.com/photos/9730025/pexels-photo-9730025.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    },
+    {
+      id: 2,
+      username:"monica",
+      fullname: "Monica Stan",
+      userImg: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      postImg: "https://images.pexels.com/photos/3497624/pexels-photo-3497624.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    },
+    {
+      id: 3,
+      username:"paul",
+      fullname: "paul smith",
+      userImg: "https://images.unsplash.com/photo-1601814933824-fd0b574dd592?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1624&q=80",
+      postImg: "https://images.unsplash.com/photo-1464802686167-b939a6910659?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80",
+    },
+    
+  ];
